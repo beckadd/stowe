fef09a1153645035d0f26804bffbe0cda167a9aa
Beck Addison <bsa10@duke.edu>
2022-09-19 21:54:22 -0400

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+  server: {
+    port: 3000,
+    watch: {
+      usePolling: true,
+    }
+  },
+  build: {
+    target: 'esnext',
+  },
+
+});
